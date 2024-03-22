b0df6faf2be52a435deef93813b2c86059b5674b
Crowdbotics <team@crowdbotics.com>
2024-03-22 16:28:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "thoughts-tracker-47248",
+  url: "https://thoughts-tracker-47248.botics.co",
+  api: "https://thoughts-tracker-47248.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
